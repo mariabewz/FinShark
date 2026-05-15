4f5c5789f7ecd0a45d77d119112c3c3547a068bb
mariabewz <mariaisabelaclash@gmail.com>
2026-05-15 15:52:40 -0300

    Controllers

--- api/Program.cs
+++ api/Program.cs
@@ -5,7 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+// builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
@@ -18,9 +20,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+   // app.UseSwagger();
+  //  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 
 app.Run();
