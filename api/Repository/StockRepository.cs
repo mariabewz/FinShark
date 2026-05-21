347c8cd89d600b36b92aeeff07de29cf7b8df566
mariabewz <mariaisabelaclash@gmail.com>
2026-05-21 17:54:05 -0300

    Pagination

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -53,7 +53,7 @@ namespace api.Repository
             {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
-            
+
             if (!string.IsNullOrWhiteSpace(query.SortBy))
             {
                 if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
@@ -63,7 +63,10 @@ namespace api.Repository
             }
 
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
