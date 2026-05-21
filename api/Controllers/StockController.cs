1e727e437342991854af69b1d99f302f8e0b84c3
mariabewz <mariaisabelaclash@gmail.com>
2026-05-21 17:32:29 -0300

    Filtering

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -24,14 +25,15 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var stocks = await _stockRepo.GetAllAsync();
-            var stockDto = stocks.Select(s => s.ToStockDto());
+            var stocks = await _stockRepo.GetAllAsync(query);
 
+            var stockDto = stocks.Select(s => s.ToStockDto());
+            
             return Ok(stockDto);
         }
 
