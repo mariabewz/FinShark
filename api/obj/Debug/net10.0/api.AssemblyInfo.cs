c1fd03a421edef0e58dc5262a68cd826ffcbad99
mariabewz <mariaisabelaclash@gmail.com>
2026-05-21 10:39:22 -0300

    To-Many CREATE (Post)

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb843cc4b826ffb0eee587b687bec47c0954e37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3228627c62978f9202af97fe1a679f79bc38c2ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
