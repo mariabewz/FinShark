ad0be03af699b65e6b38b4baf0f82232e940bef4
mariabewz <mariaisabelaclash@gmail.com>
2026-05-31 22:43:16 -0300

    Register Page

--- frontend/src/Helpers/ErrorHandler.tsx
+++ frontend/src/Helpers/ErrorHandler.tsx
@@ -4,21 +4,28 @@ import { toast } from "react-toastify";
 export const handleError = (error: any) => {
   if (axios.isAxiosError(error)) {
     var err = error.response;
-    if (Array.isArray(err?.data.errors)) {
-      for (let val of err?.data.errors) {
+    const responseData = err?.data;
+    if (Array.isArray(responseData)) {
+      for (let val of responseData) {
+        toast.warning(val.description ?? val);
+      }
+    } else if (Array.isArray(responseData?.errors)) {
+      for (let val of responseData.errors) {
         toast.warning(val.description);
       }
-    } else if (typeof err?.data.errors === "object") {
-      for (let e in err?.data.errors) {
-        toast.warning(err.data.errors[e][0]);
+    } else if (typeof responseData?.errors === "object") {
+      for (let e in responseData.errors) {
+        toast.warning(responseData.errors[e][0]);
       }
-    } else if (err?.data) {
-      toast.warning(err.data);
+    } else if (responseData) {
+      toast.warning(responseData);
     } else if (err?.status === 401) {
       toast.warning("Please login");
       window.history.pushState({}, "LoginPage", "/login");
     } else if (err) {
       toast.warning(err?.data);
+    } else {
+      toast.warning(error.message);
     }
   }
 };
